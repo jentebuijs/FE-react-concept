e13ce530cb9069fc6d5a4ee9eae213012cd38938
jentebuijs <jentebuijs@gmail.com>
2022-05-31 18:40:59 +0200

    stap 4 af

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -23,8 +23,8 @@ function Home() {
         {posts.length > 0 && posts.map((post) => {
             return (
                 <li key={post.data.subreddit}>
-                    <h3><a href={`https://www.reddit.com/r/${post.data.subreddit_id}`}>{post.data.title}</a></h3>
-                    <Link to={`/subreddit/${post.data.subreddit_id}`}>r/{post.data.subreddit}</Link>
+                    <h3><a href={`https://www.reddit.com/r/${post.data.subreddit}`}>{post.data.title}</a></h3>
+                    <Link to={`/subreddit/${post.data.subreddit}`}>r/{post.data.subreddit}</Link>
                     <p>Comments: {post.data.num_comments} - Ups: {post.data.ups}</p>
                 </li>
             );
--- src/pages/subreddit/subreddit.js
+++ src/pages/subreddit/subreddit.js
@@ -1,9 +1,39 @@
-import React from "react";
-import {useParams} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link, useParams} from "react-router-dom";
+import axios from "axios";
 
 function Subreddit() {
     const { subredditId } = useParams();
-    return console.log({subredditId});
+    const [ post, setPost ] = useState({});
+
+    useEffect(() => {
+        async function fetchPost() {
+            try {
+                const response = await axios.get(`https://www.reddit.com/r/${subredditId}/about.json`);
+                console.log(response);
+                setPost(response.data.data);
+            } catch(e) {
+                console.error(e);
+            }
+        }
+        fetchPost();
+    },[]);
+
+    return (
+        <div>
+            { Object.keys(post).length > 0 &&
+                <>
+                    <h1>{post.display_name_prefixed}</h1>
+                    <h3>Title</h3>
+                    <p>{post.title}</p>
+                    <h3>Description</h3>
+                    <p>{post.public_description}</p>
+                    <h3>Number of subscribers</h3>
+                    <p>{post.subscribers}</p>
+                    <Link to="/">Take me back</Link>
+                </> }
+        </div>
+    );
 }
 
 export default Subreddit;
