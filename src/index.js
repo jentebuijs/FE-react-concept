fc6d23fd2929fdcad2195dba654ac8d3a62c8c88
jentebuijs <jentebuijs@gmail.com>
2022-05-31 18:11:23 +0200

    stap 3 af (<a> nog niet optimaal)

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Router>
+    <Router>
+        <React.StrictMode>
             <App/>
-        </Router>
-    </React.StrictMode>
+        </React.StrictMode>
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,36 @@
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
 
 function Home() {
-    return console.log('home');
+    const [ posts, setPosts ] = useState([]);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            try {
+                const response = await axios.get('https://www.reddit.com/hot.json?limit=15');
+                console.log(response.data.data.children);
+                setPosts(response.data.data.children);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        fetchPosts();
+    }, []);
+
+    return (
+        <ul>
+        {posts.length > 0 && posts.map((post) => {
+            return (
+                <li key={post.data.subreddit}>
+                    <h3><a href={`https://www.reddit.com/r/${post.data.subreddit_id}`}>{post.data.title}</a></h3>
+                    <Link to={`/subreddit/${post.data.subreddit_id}`}>r/{post.data.subreddit}</Link>
+                    <p>Comments: {post.data.num_comments} - Ups: {post.data.ups}</p>
+                </li>
+            );
+            })}
+        </ul>
+    );
 }
 
 export default Home;
